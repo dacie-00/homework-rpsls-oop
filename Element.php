18e1491a29f5677141b77e02c5c81d30e51ae07f
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-05-22 19:30:55 +0300

    Get game to working state

--- Element.php
+++ Element.php
@@ -2,29 +2,48 @@
 
 class Element
 {
-    private string $symbol;
     private string $name;
     private array $relationships;
 
-    public function __construct(string $symbol, string $name)
+    public function __construct(string $name)
     {
-        $this->symbol = $symbol;
         $this->name = $name;
         $this->relationships = ["beats" => [], "loses" => []];
     }
 
     public function beats(Element $element): bool
     {
-        return in_array($element, $this->relationships["beats"]);
+        return isset($this->relationships["beats"][$element->name()]);
     }
 
-    public function symbol(): string
+    public function loses(Element $element): bool
     {
-        return $this->symbol;
+        return isset($this->relationships["loses"][$element->name()]);
+    }
+
+    public function setBeats(array $beats): void
+    {
+        $this->relationships["beats"] = $beats;
+    }
+
+    public function setLoses(array $loses): void
+    {
+        $this->relationships["loses"] = $loses;
     }
 
     public function name(): string
     {
         return $this->name;
+    }
+
+    public function verb(Element $opponentElement): string
+    {
+        if (isset($this->relationships["beats"][$opponentElement->name()])) {
+            return $this->relationships["beats"][$opponentElement->name()];
+        }
+        if (isset($this->relationships["loses"][$opponentElement->name()])) {
+            return $this->relationships["loses"][$opponentElement->name()];
+        }
+        return "ties";
     }
 }
--- RPSLS.php
+++ RPSLS.php
@@ -1,1 +1,107 @@
 <?php
+
+require_once "Element.php";
+
+class RPSLS {
+    private ?string $winner = null;
+
+    const PLAYER = "player";
+    const OPPONENT = "opponent";
+
+    const ROCK = "rock";
+    const PAPER = "paper";
+    const SCISSORS = "scissors";
+    private array $elements = [];
+
+    public function __construct()
+    {
+        $this->elements = [
+            self::ROCK => new Element("rock"),
+            self::PAPER => new Element("paper"),
+            self::SCISSORS => new Element("scissors")
+        ];
+        $this->elements[self::ROCK]->setBeats([self::SCISSORS => "crushes"]);
+        $this->elements[self::ROCK]->setLoses([self::PAPER => "is covered by"]);
+
+        $this->elements[self::PAPER]->setBeats([self::ROCK => "covers"]);
+        $this->elements[self::PAPER]->setLoses([self::SCISSORS => "is cut by"]);
+
+        $this->elements[self::SCISSORS]->setBeats([self::PAPER => "cut"]);
+        $this->elements[self::SCISSORS]->setLoses([self::ROCK => "is crushed by"]);
+    }
+
+    private function determineWinner(Element $playerElement, Element $opponentElement): ?string
+    {
+        if ($playerElement->beats($opponentElement)){
+            return self::PLAYER;
+        }
+        if ($playerElement->loses($opponentElement)){
+            return self::OPPONENT;
+        }
+        return null;
+    }
+
+    private function announceMatch(Element $playerElement, Element $opponentElement): void
+    {
+        $verb = $playerElement->verb($opponentElement);
+        echo ucfirst("{$playerElement->name()} $verb {$opponentElement->name()}!\n");
+    }
+
+    private function announceWinner()
+    {
+        switch ($this->winner) {
+            case self::PLAYER:
+                echo "You won!\n";
+                return;
+            case self::OPPONENT:
+                echo "You lost!\n";
+                return;
+            default:
+                echo "It's a tie!\n";
+        }
+    }
+
+    private function announcePicks(Element $playerElement, Element $opponentElement): void
+    {
+        echo "You picked {$playerElement->name()}!\n";
+        echo "Opponent picked {$opponentElement->name()}!\n";
+    }
+
+    public function play(Element $playerElement, Element $opponentElement): void
+    {
+        $this->winner = $this->determineWinner($playerElement, $opponentElement);
+        $this->announcePicks($playerElement, $opponentElement);
+        $this->announceMatch($playerElement, $opponentElement);
+        $this->announceWinner();
+    }
+
+    public function getElements(): array
+    {
+        return $this->elements;
+    }
+}
+
+$RPSLS = new RPSLS();
+$possibleElements = array_keys($RPSLS->getElements());
+echo "Pick an element! ";
+echo "(";
+foreach ($possibleElements as $index => $element) {
+    if ($index != 0) {
+        echo ", ";
+    }
+    echo "$element";
+}
+echo ")\n";
+
+while(true) {
+    $userElement = strtolower(readline("Element - "));
+    if (!in_array($userElement, $possibleElements)) {
+        echo "Invalid element!";
+        continue;
+    }
+    break;
+}
+
+$opponentElement = $RPSLS->getElements()[array_rand($RPSLS->getElements())];
+
+$RPSLS->play($RPSLS->getElements()[$userElement], $opponentElement);
